49248443d18ceb167e29e245a6bafe5becd133b3
Muhammad Karim <muhammadkarim10@gmail.com>
2020-03-31 00:01:39 +0700

    upload file

---
+++ crud_aspdotnet_ajax_ef/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crud_aspdotnet_ajax_ef.Models;
+using System.Data.Entity;
+namespace crud_aspdotnet_ajax_ef.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Getdata()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Save successfully!!" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Update successfully!!" }, JsonRequestBehavior.AllowGet);
+                }
+
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Delete successfully!!" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ crud_aspdotnet_ajax_ef/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="crud_aspdotnet_ajax_ef.MvcApplication" Language="C#" %>
